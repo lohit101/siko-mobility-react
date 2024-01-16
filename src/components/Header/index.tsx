a53b46a5f57ef8e0e1e3f49b274577d7d3b0820c
lohit101 <lohitchhabra101@gmail.com>
2024-01-16 23:00:52 +0530

    16-02-2024 - Final fixes - Changed favicon

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -14,7 +14,7 @@ const Header: React.FC = () => {
             </div>
             <h2 className="w-full text-4xl sm:text-6xl font-bold capitalize text-center">guichet unique pour les professionnels</h2>
             <p className="text-2xl my-5 text-center">Financement, Assurance, Entretien Et Garantie.</p>
-            <a href='#services' className="bg-blue-500 text-white py-2 px-5 rounded-full self-center">Notre Service Pour Toutes Les Mobilités &#129123;</a>
+            <a href='#services' className="bg-blue-500 text-white py-2 px-5 rounded-full self-center">Notre Service Pour Toutes Les Mobilités &nbsp; &darr;</a>
         </div>
     )
 }
